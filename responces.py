8ffc9a59e75e520cc7dc8acf4a9a68c2718d9056
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-07-27 00:18:32 -0400

    added more responses

---
+++ responces.py
@@ -0,0 +1,17 @@
+class MESSAGE_ALERTS:
+    MESSAGE_ERROR_MESSAGE_TYPE_ONE = "Rut Roh! Look's like your message failed to send! Please try again";
+    MESSAGE_ERROR_MESSAGE_TYPE_TWO = "Whoopsy! Your messaged failed to send! Please try again";
+    MESSAGE_ERROR_MESSAGE_TYPE_THREE = "Hmmm... that wasnt supposed to happen. Your message failed to send! Please try again";
+
+class GIFT_ALERTS:
+    GIFT_MESSAGE_TYPE_ONE = "We are sorry for the inconvenience. Please accept this gift as a token of our gratitude. \n ~ Luv - The Rocketeer Developer Team";
+    GIFT_MESSAGE_TYPE_TWO = "HOORAY! You have received a gift from the Rocketeer Developer Team! \n\t ~ Luv - The Rocketeer Developer Team";
+    GIFT_MESSAGE_TYPE_THREE = "Happy Birthday! The Rocketeer Team wants to give you a month of free premium! Enjoy! \n\t ~ Luv - The Rocketeer Developer Team";
+
+
+
+class SYSTEM_ALERTS: 
+    SYSTEM_ALERT_MESSAGE_TYPE_DOWN_TIME = "Yo yo yo! We are going to be down for matience on {} at {}. Just wanted to let ya know ;)";
+    SYSTEM_ALERT_MESSAGE_TYPE_REPORTED_WARNING = "Whoah there! Slow your roll dude! We've gotten a report of a {} incident. We'll be back up soon!";
+    SYSTEM_ALERT_MESSAGE_TYPE_REPORTED_TEMP_SUSPEND = "Well... it happens. You got a {} day suspension. You were reported multible times due to {}. So uhh yea... dont do that anymore :) \n-Cheers from the Rocketeer Moderation Staff"
+    SYSTEM_ALERT_MESSAGE_TYPE_PERM_SUSPENDED = "WHAT THE FLIP? You got a permanent suspension. You were reported multiple times due to {}."
